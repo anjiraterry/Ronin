d9c077c55a68af0ff6e138703bf9d380272ced5e
Anjira Terry <anjiraterry@gmail.com>
2022-07-11 18:31:50 -0700

    second commit

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Featured from '../../components/Featured'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Head>
-        <title>Ronin</title>
-        <meta name="description" content="SLAY!!!" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+       <Head>
+         <title>Ronin</title>
+         <meta name="description" content="SLAY!!!" />
+         <link rel="icon" href="/favicon.ico" />
+       </Head>
+       <Featured/>
+       <ProductList/>
 
      
     </div>
---
+++ pages/product/[id].jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from ".../styles/Product.module.css"
+import Image from "next/image"
+
+const Product = () => {
+  return (
+    <div className={styles.container}>Product</div>
+  )
+}
+
+export default Product;
